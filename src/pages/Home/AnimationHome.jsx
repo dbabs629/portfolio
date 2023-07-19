37714325e41a91f00b7138c061e3c96c1b44eeb9
daniel <daniel.l.babin@gmail.com>
2023-07-19 11:54:59 -0400

    third commit

--- src/pages/Home/AnimationHome.jsx
+++ src/pages/Home/AnimationHome.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 function AnimationHome() {
-  const [textPosition, setTextPosition] = useState(0)
+  // const [textPosition, setTextPosition] = useState(0)
 
   let skillsList = [
     'HTML',
