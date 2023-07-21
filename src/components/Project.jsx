8a99dd14cb95d5fa7407dbd2966c4b74cf49b01d
daniel <daniel.l.babin@gmail.com>
2023-07-21 10:14:18 -0400

    worked on project section layout

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,23 +3,33 @@ import LoadImg from './LoadImg'
 
 function Project({ title, text, projectLowResImg, projectHighResImg, alt }) {
   return (
-    <div className='w-full flex justify-center border-2'>
-      <div className='flex flex-col sm:w-full border-2'>
-        <h3 className='text-3xl text-primary'>{title}</h3>
-        <p className='text-gray-800'>{text}</p>
-        <Link
-          className='w-32 p-2 text-center bg-blue-500 text-white font-semibold rounded-lg hover:bg-orange-500'
-          to='project'>
-          View project
-        </Link>
-      </div>
-      <div className='w-full border'>
-        <LoadImg
-          imgLowRes={projectLowResImg}
-          imgHighRes={projectHighResImg}
-          addClassName='max-w-full w-full max-h-full object-cover rounded-lg'
-          alt={alt}
-        />
+    <div className='w-full lg:h-screen flex items-center'>
+      <div className='max-w-[500px] lg:max-w-[1000px] w-full flex flex-col items-center mx-auto lg:flex-row my-16 lg:space-x-8 lg:space-y-0 lg'>
+        <div className='lg:hidden w-full'>
+          <LoadImg
+            imgLowRes={projectLowResImg}
+            imgHighRes={projectHighResImg}
+            addClassName='max-w-full w-full max-h-full object-cover rounded-lg'
+            alt={alt}
+          />
+        </div>
+        <div className='w-full flex flex-col space-y-8 sm:w-full'>
+          <h3 className='text-3xl text-primary'>{title}</h3>
+          <p className='text-gray-800'>{text}</p>
+          <Link
+            className='w-32 p-2 text-center bg-blue-500 text-white font-semibold rounded-lg hover:bg-orange-500'
+            to='project'>
+            View project
+          </Link>
+        </div>
+        <div className='hidden w-full lg:block lg:max-w-[800px]'>
+          <LoadImg
+            imgLowRes={projectLowResImg}
+            imgHighRes={projectHighResImg}
+            addClassName='max-w-full w-full max-h-full object-cover rounded-lg'
+            alt={alt}
+          />
+        </div>
       </div>
     </div>
   )
--- src/pages/Home/ProjectSection.jsx
+++ src/pages/Home/ProjectSection.jsx
@@ -4,8 +4,8 @@ import projectHighResImg from '../../assets/images/webdev-mocksite.PNG'
 
 function ProjectSection() {
   return (
-    <section id='projects' className='bg-blue-50 py-16'>
-      <div className='w-2/3 mx-auto flex flex-col items-center justify-center space-y-24'>
+    <section id='projects' className='bg-blue-50'>
+      <div className='w-full mx-auto flex flex-col items-center justify-center py-24 px-16'>
         <Project
           projectLowResImg={projectLowResImg}
           projectHighResImg={projectHighResImg}
@@ -25,13 +25,6 @@ function ProjectSection() {
           projectHighResImg={projectHighResImg}
           alt='project alt txt'
           title='Family Tree Web App'
-          text={`Communication is at the heart of my work. I thrive in collaborative environments, actively engaging with team members and clients to understand their needs and goals. My ability to translate complex technical jargon into clear, accessible language fosters effective communication throughout the development process. Empathy plays a significant role in my approach to design. Understanding the end-users' perspectives allows me to craft intuitive interfaces that resonate with their needs and preferences. This user-centered mindset guides me in creating seamless and enjoyable interactions that enhance the overall user experience.`}
-        />
-        <Project
-          projectLowResImg={projectLowResImg}
-          projectHighResImg={projectHighResImg}
-          alt='project alt txt'
-          title='Ecommerce'
           text={`Communication is at the heart of my work. I thrive in collaborative environments, actively engaging with team members and clients to understand their needs and goals. My ability to translate complex technical jargon into clear, accessible language fosters effective communication throughout the development process. Empathy plays a significant role in my approach to design. Understanding the end-users' perspectives allows me to craft intuitive interfaces that resonate with their needs and preferences. This user-centered mindset guides me in creating seamless and enjoyable interactions that enhance the overall user experience.`}
         />
       </div>
