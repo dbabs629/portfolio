cae982f2878d786979d286657b04097cc9c7c00a
daniel <daniel.l.babin@gmail.com>
2023-10-11 15:02:35 -0400

    changed spinner icon in suspense fallback to svg for faster load time for first paint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/Home/HomePage'
+const HomePage = lazy(() => import('./pages/Home/HomePage'))
 const Footer = lazy(() => import('../src/components/Footer'))
 const IconsList = lazy(() => import('./components/IconsList'))
 const Nav = lazy(() => import('../src/components/nav/Nav'))
@@ -15,10 +15,21 @@ function App() {
     <>
       <Suspense
         fallback={
-          <div className='flex h-screen w-screen items-center justify-center'>
-            <h2 className='animate-bounce animate-pulse text-4xl font-bold text-red'>
-              Loading...
+          <div className='flex h-screen w-screen items-center justify-center space-x-4'>
+            <h2 className='animate-pulse text-4xl font-bold text-red'>
+              Loading
             </h2>
+            <div className='h-[10%] w-[10%] animate-pulse'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                height='1em'
+                viewBox='0 0 512 512'
+                fill='#74C7FE'
+                className='h-full w-full animate-spin'>
+                {/*! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --*/}
+                <path d='M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z' />
+              </svg>
+            </div>
           </div>
         }>
         <Nav />
